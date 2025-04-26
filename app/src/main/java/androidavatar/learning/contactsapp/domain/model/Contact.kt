b97a8de83b6ad73bee60d5737dd5506292d8a721
hegde10122 <hegde10122@gmail.com>
2025-04-27 00:35:44 +0530

    changes

---
+++ app/src/main/java/androidavatar/learning/contactsapp/domain/model/Contact.kt
@@ -0,0 +1,7 @@
+package androidavatar.learning.contactsapp.domain.model
+
+data class Contact(
+    val id: Int,
+    val name: String,
+    val phoneNumber: String
+)
--- app/src/main/java/androidavatar/learning/contactsapp/presentation/screens/ContactsListScreen.kt
+++ app/src/main/java/androidavatar/learning/contactsapp/presentation/screens/ContactsListScreen.kt
@@ -1,9 +1,13 @@
 package androidavatar.learning.contactsapp.presentation.screens
 
+import androidavatar.learning.contactsapp.domain.model.Contact
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -12,20 +16,39 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 
 @Composable
 fun ContactsListScreen(navController: NavController){
 
+    val contacts = listOf(
+        Contact(1, "John Doe", "123-456-7890"),
+        Contact(2, "Jane Smith", "987-654-3210"),
+        Contact(3, "Michael Brown", "456-789-0123")
+    )
 
     Column (
         modifier = Modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
-
     ){
-        Text(text="This is the Contacts List Screen",
+        Text(
+            text = "Contacts",
             style=MaterialTheme.typography.headlineMedium,
-            modifier = Modifier.padding(bottom = 16.dp))
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        LazyColumn {
+            items(contacts){
+                contact -> ContactItem(contact)
+                HorizontalDivider()
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
 
         Button(onClick = {
             navController.popBackStack()
@@ -34,10 +57,18 @@ fun ContactsListScreen(navController: NavController){
         }
     }
 
-
 }
 
+@Composable
+fun ContactItem(contact: Contact) {
 
+    Column(
+        modifier = Modifier.padding(8.dp)
+    ){
+        Text(text = contact.name, style = MaterialTheme.typography.bodySmall)
+        Text(text = contact.phoneNumber, style=MaterialTheme.typography.bodyLarge)
+    }
+}
 
 
 /**
@@ -70,3 +101,11 @@ Text("Back to Welcome")
 }
 }
  */
+
+/**
+val contacts = listOf(
+Contact(1, "John Doe", "123-456-7890"),
+Contact(2, "Jane Smith", "987-654-3210"),
+Contact(3, "Michael Brown", "456-789-0123")
+)
+ * */
